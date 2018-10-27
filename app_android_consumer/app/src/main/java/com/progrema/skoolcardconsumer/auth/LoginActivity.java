dbc526c284bbefeec8d79370efb1492e73abbba4
Progrema Studio <progrema.studio@gmail.com>
2018-10-28 05:28:03 +0700

    Porting login page

---
+++ app_android_consumer/app/src/main/java/com/progrema/skoolcardconsumer/auth/LoginActivity.java
@@ -0,0 +1,38 @@
+package com.progrema.skoolcardconsumer.auth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.progrema.skoolcardconsumer.MainActivity;
+import com.progrema.skoolcardconsumer.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button registrationButton = findViewById(R.id.registration_button);
+        registrationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
