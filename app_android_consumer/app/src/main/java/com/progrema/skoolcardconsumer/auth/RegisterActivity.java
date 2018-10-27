eb246f5e8ce8be1437c4d3f482b9f9bd26cad653
Progrema Studio <progrema.studio@gmail.com>
2018-10-28 05:46:23 +0700

    Adding register page

---
+++ app_android_consumer/app/src/main/java/com/progrema/skoolcardconsumer/auth/RegisterActivity.java
@@ -0,0 +1,38 @@
+package com.progrema.skoolcardconsumer.auth;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.progrema.skoolcardconsumer.MainActivity;
+import com.progrema.skoolcardconsumer.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button registerButton = findViewById(R.id.registration_button);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
