20b201c7153db749493680cf117d8e272d7a6e44
Progrema Studio <progrema.studio@gmail.com>
2018-10-29 21:15:46 +0800

    Adding logout function

--- app_android_consumer/app/src/main/java/com/progrema/skoolcardconsumer/HomeActivity.java
+++ app_android_consumer/app/src/main/java/com/progrema/skoolcardconsumer/HomeActivity.java
@@ -1,11 +1,16 @@
 package com.progrema.skoolcardconsumer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.progrema.skoolcardconsumer.auth.LoginActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -41,4 +46,22 @@ public class HomeActivity extends AppCompatActivity {
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.logout) {
+            FirebaseAuth.getInstance().signOut();
+            startActivity(new Intent(this, LoginActivity.class));
+            finish();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
 }
