b8085a3ca8b1ce63e603a7cf1f96cadfcbc318fd
Progrema Studio <progrema.studio@gmail.com>
2018-10-31 06:36:57 +0800

    Better layout of history table

--- app_android_merchant/app/src/main/java/com/progrema/skoolcardmerchant/core/history/TransactionContent.java
+++ app_android_merchant/app/src/main/java/com/progrema/skoolcardmerchant/core/history/TransactionContent.java
@@ -12,49 +12,21 @@ public class TransactionContent {
     public static final List<Transaction> ITEMS = new ArrayList<>();
 
     static {
-
-        ITEMS.add(Transaction.create()
-                .setInvoice("20181101-001-001")
-                .setAmount("Rp 50,000.00")
-                .setTimestamp("2018-10-01")
-                .setMerchant("123456")
-                .setConsumer("123456")
-                .setChild("123456")
-                .setState(Transaction.OPEN)
-                .setProducts(new Product[]{
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture(""),
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture(""),
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture("")
-                }));
-
-        ITEMS.add(Transaction.create()
-                .setInvoice("20181101-001-002")
-                .setAmount("Rp 50,000.00")
-                .setTimestamp("2018-10-01")
-                .setMerchant("123456")
-                .setConsumer("123456")
-                .setChild("123456")
-                .setState(Transaction.OPEN)
-                .setProducts(new Product[]{
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture(""),
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture(""),
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture("")
-                }));
-
-        ITEMS.add(Transaction.create()
-                .setInvoice("20181101-001-002")
-                .setAmount("Rp 50,000.00")
-                .setTimestamp("2018-10-01")
-                .setMerchant("123456")
-                .setConsumer("123456")
-                .setChild("123456")
-                .setState(Transaction.OPEN)
-                .setProducts(new Product[]{
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture(""),
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture(""),
-                        Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture("")
-                }));
-
+        for (int i=0; i<10; i++) {
+            ITEMS.add(Transaction.create()
+                    .setInvoice("20181101-154-023")
+                    .setAmount("Rp 50,000.00")
+                    .setTimestamp("2018-10-01")
+                    .setMerchant("123456")
+                    .setConsumer("123456")
+                    .setChild("123456")
+                    .setState(Transaction.OPEN)
+                    .setProducts(new Product[]{
+                            Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture(""),
+                            Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture(""),
+                            Product.create().setName("Biscuit").setPrice("Rp 10,500.00").setPicture("")
+                    }));
+        }
     }
 
 }
