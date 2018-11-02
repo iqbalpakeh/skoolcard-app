b93572d1262aaba7d395c2eaa4600a1cfa53a4f8
Progrema Studio <progrema.studio@gmail.com>
2018-11-03 06:19:00 +0700

    Remove unused import

--- app_android_merchant/app/src/main/java/com/progrema/skoolcardmerchant/core/auth/LoginActivity.java
+++ app_android_merchant/app/src/main/java/com/progrema/skoolcardmerchant/core/auth/LoginActivity.java
@@ -10,8 +10,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.progrema.skoolcardmerchant.core.HomeActivity;
 import com.progrema.skoolcardmerchant.R;
 import com.progrema.skoolcardmerchant.api.firebase.FbAuth;
