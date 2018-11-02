869539ad158e735f2aae41e1e31549504bfe231a
Progrema Studio <progrema.studio@gmail.com>
2018-11-02 11:33:02 +0800

    Change different type of product

--- app_android_merchant/app/src/main/java/com/progrema/skoolcardmerchant/core/shop/ProductContent.java
+++ app_android_merchant/app/src/main/java/com/progrema/skoolcardmerchant/core/shop/ProductContent.java
@@ -11,11 +11,11 @@ public class ProductContent {
 
     static {
         ITEMS.add(Product.create().setName("Sandwich").setPrice("Rp 10,500.00").setPicture(""));
-        ITEMS.add(Product.create().setName("Sandwich").setPrice("Rp 10,500.00").setPicture(""));
-        ITEMS.add(Product.create().setName("Sandwich").setPrice("Rp 10,500.00").setPicture(""));
-        ITEMS.add(Product.create().setName("Sandwich").setPrice("Rp 10,500.00").setPicture(""));
-        ITEMS.add(Product.create().setName("Sandwich").setPrice("Rp 10,500.00").setPicture(""));
-        ITEMS.add(Product.create().setName("Sandwich").setPrice("Rp 10,500.00").setPicture(""));
+        ITEMS.add(Product.create().setName("Hamburger").setPrice("Rp 9,500.00").setPicture(""));
+        ITEMS.add(Product.create().setName("Fried Fries").setPrice("Rp 8,500.00").setPicture(""));
+        ITEMS.add(Product.create().setName("Popcorn").setPrice("Rp 7,500.00").setPicture(""));
+        ITEMS.add(Product.create().setName("Donut").setPrice("Rp 6,500.00").setPicture(""));
+        ITEMS.add(Product.create().setName("Chips").setPrice("Rp 5,500.00").setPicture(""));
     }
 
 }
