066291ef495075105e3ce70be0e4d6be20def9b4
Progrema Studio <progrema.studio@gmail.com>
2018-11-10 15:50:17 +0700

    Update sample script

--- app_api/shell-script.ts
+++ app_api/shell-script.ts
@@ -1,41 +1,17 @@
-// Paste this script on function shell for debugging purpose
-
-//var input = { uid: "kjypVYRbNIP6jqGONdDaNDzRNb02", amount: "50" };
-
 var input = {
-  amount: "1550",
-  child: "DUMMY_CHILD",
+  amount: "250",
+  child: "xxx",
   consumer: "kjypVYRbNIP6jqGONdDaNDzRNb02",
-  invoice: "DUMMY_INVOICE_1",
   merchant: "bbrEquzl57RpZI5qiZhtcXVqlnG3",
   products: [
-    {
-      name: "Sandwich",
-      number: "1",
-      picture: "dummy.jpg",
-      price: "150"
-    },
     {
       name: "Hamburger",
       number: "1",
       picture: "dummy.jpg",
       price: "250"
-    },
-    {
-      name: "Fried Fries",
-      number: "1",
-      picture: "dummy.jpg",
-      price: "350"
-    },
-    {
-      name: "Popcorn",
-      number: "2",
-      picture: "dummy.jpg",
-      price: "400"
     }
   ],
-  state: "open",
-  timestamp: "DUMMY_TIMESTAMP"
+  state: "open"
 };
 
 doPayment(input);
