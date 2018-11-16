2de58e32d3873f7e28ca77de1a764eaca813a674
Progrema Studio <progrema.studio@gmail.com>
2018-11-17 05:28:47 +0700

    Importing dashboard to project

--- app_web/admin/src/App.js
+++ app_web/admin/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Login from "./containers/login/Login";
+import Dashboard from "./containers/dashboard/Dashboard";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Login />
+        <Dashboard />
       </div>
     );
   }
