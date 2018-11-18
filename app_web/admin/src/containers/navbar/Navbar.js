27004184ebd14d8eeb7c165c3a9e08bc785f63e1
Progrema Studio <progrema.studio@gmail.com>
2018-11-19 04:54:44 +0700

    Rename the app name

--- app_web/admin/src/containers/navbar/Navbar.js
+++ app_web/admin/src/containers/navbar/Navbar.js
@@ -22,7 +22,7 @@ class Navbar extends Component {
       <div>
         <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
           <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#top">
-            Company name
+            Skoolcard Admin
           </a>
           <input
             className="form-control form-control-dark w-100"
