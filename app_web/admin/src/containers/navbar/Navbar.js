7ea04a12c7044e08864dfe6d738c6dd0668ec646
Progrema Studio <progrema.studio@gmail.com>
2018-11-19 19:01:23 +0800

    remove div on navbar

--- app_web/admin/src/containers/navbar/Navbar.js
+++ app_web/admin/src/containers/navbar/Navbar.js
@@ -19,26 +19,24 @@ class Navbar extends Component {
 
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-          <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#top">
-            Skoolcard Admin
-          </a>
-          <input
-            className="form-control form-control-dark w-100"
-            type="text"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <ul className="navbar-nav px-3">
-            <li className="nav-item text-nowrap">
-              <a className="nav-link" href="#top" onClick={this.handleSignout}>
-                Sign out
-              </a>
-            </li>
-          </ul>
-        </nav>
-      </div>
+      <nav className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+        <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#top">
+          Skoolcard Admin
+        </a>
+        <input
+          className="form-control form-control-dark w-100"
+          type="text"
+          placeholder="Search"
+          aria-label="Search"
+        />
+        <ul className="navbar-nav px-3">
+          <li className="nav-item text-nowrap">
+            <a className="nav-link" href="#top" onClick={this.handleSignout}>
+              Sign out
+            </a>
+          </li>
+        </ul>
+      </nav>
     );
   }
 }
