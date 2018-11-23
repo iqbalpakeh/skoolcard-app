0627ac1a4d3b59620ff2348e36adad48b39515e8
Progrema Studio <progrema.studio@gmail.com>
2018-11-24 05:35:37 +0700

    Simplified code

--- app_web/admin/src/containers/Dashboard.js
+++ app_web/admin/src/containers/Dashboard.js
@@ -7,37 +7,33 @@ import * as api from "../Api";
 import Table from "./Table";
 import { Line } from "react-chartjs-2";
 
+const tableOptions = {
+  label: "My First dataset",
+  fill: false,
+  lineTension: 0.1,
+  backgroundColor: "rgba(75,192,192,0.4)",
+  borderColor: "rgba(75,192,192,1)",
+  borderCapStyle: "butt",
+  borderDash: [],
+  borderDashOffset: 0.0,
+  borderJoinStyle: "miter",
+  pointBorderColor: "rgba(75,192,192,1)",
+  pointBackgroundColor: "#fff",
+  pointBorderWidth: 1,
+  pointHoverRadius: 5,
+  pointHoverBackgroundColor: "rgba(75,192,192,1)",
+  pointHoverBorderColor: "rgba(220,220,220,1)",
+  pointHoverBorderWidth: 2,
+  pointRadius: 1,
+  pointHitRadius: 10
+};
+
 export default class Dashboard extends Component {
   constructor(props) {
     super(props);
     this.state = {
       datas: [],
-      chardData: {
-        labels: [],
-        datasets: [
-          {
-            label: "My First dataset",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(75,192,192,0.4)",
-            borderColor: "rgba(75,192,192,1)",
-            borderCapStyle: "butt",
-            borderDash: [],
-            borderDashOffset: 0.0,
-            borderJoinStyle: "miter",
-            pointBorderColor: "rgba(75,192,192,1)",
-            pointBackgroundColor: "#fff",
-            pointBorderWidth: 1,
-            pointHoverRadius: 5,
-            pointHoverBackgroundColor: "rgba(75,192,192,1)",
-            pointHoverBorderColor: "rgba(220,220,220,1)",
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            data: []
-          }
-        ]
-      }
+      chardData: {}
     };
   }
 
@@ -54,29 +50,7 @@ export default class Dashboard extends Component {
                 return new Date(data).toLocaleDateString();
               })
               .reverse(),
-            datasets: [
-              {
-                label: "My First dataset",
-                fill: false,
-                lineTension: 0.1,
-                backgroundColor: "rgba(75,192,192,0.4)",
-                borderColor: "rgba(75,192,192,1)",
-                borderCapStyle: "butt",
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: "miter",
-                pointBorderColor: "rgba(75,192,192,1)",
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 1,
-                pointHoverRadius: 5,
-                pointHoverBackgroundColor: "rgba(75,192,192,1)",
-                pointHoverBorderColor: "rgba(220,220,220,1)",
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: datas.totals.reverse()
-              }
-            ]
+            datasets: [{ ...tableOptions, ...{ data: datas.totals.reverse() } }]
           }
         });
       })
